e848e035663bb6551a9cfe0bcfabd42f992e6187
SubarnaSaha08 <jucse29.subarna.349@gmail.com>
2024-10-26 18:08:02 +0600

    fix: rectify login controller and webpage

--- jumcms/users/forms.py
+++ jumcms/users/forms.py
@@ -3,12 +3,15 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 from .constants import ROLE_CHOICES, BLOOD_GROUP_CHOICES, GENDER_CHOICES
 from .models import User
+
+
 class UserRegistrationForm(UserCreationForm):
-    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Password confirm', widget=forms.PasswordInput)
-    role = forms.ChoiceField(choices = ROLE_CHOICES, label = 'Role')
-    blood_group = forms.ChoiceField(choices= BLOOD_GROUP_CHOICES, label = 'Blood group')
-    gender = forms.ChoiceField(choices = GENDER_CHOICES, label = 'Gender')
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Password confirm", widget=forms.PasswordInput)
+    role = forms.ChoiceField(choices=ROLE_CHOICES, label="Role")
+    blood_group = forms.ChoiceField(choices=BLOOD_GROUP_CHOICES, label="Blood group")
+    gender = forms.ChoiceField(choices=GENDER_CHOICES, label="Gender")
+
     class Meta:
         model = User
         fields = [
@@ -21,7 +24,8 @@ class UserRegistrationForm(UserCreationForm):
             "phone_number",
         ]
 
+
 class LoginForm(AuthenticationForm):
     class Meta:
         model = User
-        fields = ('email', 'password')
+        fields = ("email", "password")
