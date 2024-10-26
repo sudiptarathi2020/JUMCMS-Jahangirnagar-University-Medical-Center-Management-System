38909594ce6de73533d31ec0b5aad2ef56c8686c
SubarnaSaha08 <jucse29.subarna.349@gmail.com>
2024-10-26 14:16:25 +0600

    refactor: rectify user model

--- jumcms/users/models.py
+++ jumcms/users/models.py
@@ -1,64 +1,13 @@
 from django.db import models
 from django.core.validators import RegexValidator
-from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser
 from users.constants import *
-import re
-
-
-class UserManager(BaseUserManager):
-    def create_user(
-        self,
-        role_id,
-        email,
-        name,
-        role,
-        blood_group,
-        gender,
-        date_of_birth,
-        phone_number,
-        profile_picture="profilePictures/default.png",
-        password=None,
-        confirmed_password=None,
-    ):
-
-        if not email:
-            raise ValueError("Users must have an email address")
-        user = self.model(
-            role_id=role_id,
-            email=self.normalize_email(email),
-            name=name,
-            role=role,
-            blood_group=blood_group,
-            date_of_birth=date_of_birth,
-            gender=gender,
-            phone_number=phone_number,
-            profile_picture=profile_picture,
-        )
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(
-        self, email, name, phone_number, password=None, confirmed_password=None
-    ):
-        user = self.create_user(
-            email=self.normalize_email(email),
-            name=name,
-        )
-        user.set_password(password)
-        user.is_admin = True
-        user.save(using=self._db)
-        return user
 
 
 class User(AbstractBaseUser):
     phone_number_validator = RegexValidator(
         regex=r"^\+880\d{10}$",
         message="Phone number must start with '+880' and be followed by 10 digits.",
-    )
-    role_id = models.CharField(
-        max_length=255,
-        unique=True,
     )
     email = models.EmailField(
         verbose_name="Email",
@@ -70,42 +19,35 @@ class User(AbstractBaseUser):
         max_length=200,
         choices=ROLE_CHOICES,
     )
-    blood_group = (
-        models.CharField(
-            max_length=200,
-            choices=BLOOD_GROUP_CHOICES,
-        ),
+    blood_group = models.CharField(
+        max_length=200,
+        choices=BLOOD_GROUP_CHOICES,
     )
-    date_of_birth = models.DateField(auto_now_add=True)
-    gender = (
-        models.CharField(
-            max_length=200,
-            choices=GENDER_CHOICES,
-        ),
+    date_of_birth = models.DateField()
+    gender = models.CharField(
+        max_length=200,
+        choices=GENDER_CHOICES,
     )
     phone_number = models.CharField(
-        max_length=200,
+        max_length=15,
         validators=[phone_number_validator],
     )
     profile_picture = models.ImageField(
         upload_to="profilePictures/", default="profilePictures/default.png"
     )
-    is_active = models.BooleanField(default=True)
+    is_approved = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    objects = UserManager()
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = [
         "name",
-        "role_id",
         "role",
         "blood_group",
         "date_of_birth",
         "gender",
         "phone_number",
-        "profile_picture",
     ]
 
     def __str__(self):
