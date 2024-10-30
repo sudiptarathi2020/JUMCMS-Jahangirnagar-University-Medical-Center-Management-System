e6aa0d01f5a637b721739d2b94a1bc510e352f62
SubarnaSaha08 <jucse29.subarna.349@gmail.com>
2024-10-30 21:05:27 +0600

    fix: doxtor dashboard information

--- jumcms/appointments/controllers.py
+++ jumcms/appointments/controllers.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, render, redirect
+from users.models import Doctor
 
 # Create your views here.
+
+
+# Doctor part start
+@login_required
+def get_doctor_appointments(request):
+    pass
+
+
+# Doctor part end
--- jumcms/appointments/models.py
+++ jumcms/appointments/models.py
@@ -1,37 +1,41 @@
 from django.db import models
+from appointments.constants import *
 from users.models import Doctor, Patient, LabTechnician
 
 
-# Create your models here.
-class DoctorAppointment(models.Model):
+class Appointment(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    appointment_date_time = models.DateTimeField()
+    status = models.CharField(
+        max_length=20, choices=[("scheduled", "Scheduled"), ("completed", "Completed")]
+    )
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return f"Appointment on {self.appointment_date_time}"
+
+
+class DoctorAppointment(Appointment):
     doctor = models.ForeignKey(
         Doctor, on_delete=models.CASCADE, related_name="doctor_appointments"
     )
-    patient = models.ForeignKey(
-        Patient, on_delete=models.CASCADE, related_name="doctor_appointments"
-    )
-    appointment_date_time = models.DateTimeField()
-    status = models.CharField(
-        max_length=20, choices=[("scheduled", "Scheduled"), ("completed", "Completed")]
-    )
+    reason = models.CharField(max_length=200, default=False)
 
     def __str__(self):
-        return (
-            f"Appointment with Dr. {self.doctor.user.name} on {self.appointment_date}"
-        )
+        return f"Appointment with Dr. {self.doctor.user.name} on {self.appointment_date_time}"
 
 
-class TestAppointment(models.Model):
+class TestAppointment(Appointment):
     lab_technician = models.ForeignKey(
         LabTechnician, on_delete=models.CASCADE, related_name="test_appointments"
     )
-    patient = models.ForeignKey(
-        Patient, on_delete=models.CASCADE, related_name="test_appointments"
-    )
-    appointment_date_time = models.DateTimeField()
-    status = models.CharField(
-        max_length=20, choices=[("scheduled", "Scheduled"), ("completed", "Completed")]
+    medical_test = models.CharField(
+        max_length=200,
+        choices=MEDICAL_TEST_CHOICES,
+        default=False,
     )
 
     def __str__(self):
-        return f"Appointment with {self.lab_technician.user.username} on {self.appointment_date}"
+        return f"Appointment with {self.lab_technician.user.username} on {self.appointment_date_time} for {self.medical_test}"
