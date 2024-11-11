6c9e1ed5243f9cdaaae2a55d30e55c81ef249547
sudiptarathi2020 <rathisudipta99@gmail.com>
2024-11-11 22:30:03 +0600

    update test_urls.py in medicines.tests

--- jumcms/medicines/tests/test_urls.py
+++ jumcms/medicines/tests/test_urls.py
@@ -10,25 +10,25 @@ class TestUrls(SimpleTestCase):
         Testing of all prescriptions url
         """
         url = reverse('medicines:all_prescriptions')
-        self.assertEquals(resolve(url).func, all_prescriptions)
+        self.assertEqual(resolve(url).func, all_prescriptions)
 
     def test_search_prescriptions_url_is_resolved(self):
         """
         Testing of search prescriptions url
         """
         url = reverse('medicines:search-prescriptions')
-        self.assertEquals(resolve(url).func, search_prescriptions)
+        self.assertEqual(resolve(url).func, search_prescriptions)
 
     def test_prescription_details_url_is_resolved(self):
         """
         Testing of prescription details url
         """
         url = reverse('medicines:prescription-details', args=[1])
-        self.assertEquals(resolve(url).func, prescription_details)
+        self.assertEqual(resolve(url).func, prescription_details)
 
     def test_dispense_medicines_url_is_resolved(self):
         """
         Testing of dispense_medicines url
         """
         url = reverse('medicines:dispense-medicines', args=[1])
-        self.assertEquals(resolve(url).func, dispense_medicines)
+        self.assertEqual(resolve(url).func, dispense_medicines)
