cf10bdeada31d51c35128b8e9a0198d723f0fc57
sudiptarathi2020 <rathisudipta99@gmail.com>
2025-07-17 14:07:39 +0600

    further restrucred the repo

---
+++ backend/core/jumcms/settings/templates/settings.dev.py
@@ -0,0 +1,3 @@
+SECRET_KEY = "django-insecure-+5@v2_e=w9+%xz3xmr&inw-0c%af4r8e&uu+__0y27x6ysf^o-"
+DEBUG = True
+
--- backend/core/jumcms/urls.py
+++ backend/core/jumcms/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from users.controllers import home
@@ -21,6 +21,9 @@ from users.controllers import home
 from django.conf import settings
 from django.conf.urls.static import static
 from users.controllers import log_in
+
+print(settings.DEBUG)
+print(settings.SECRET_KEY)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
